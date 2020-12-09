75dd3fe84d63ee00146c34a330c2952fac2b343a
devops Devops <devops61a@outlook.com>
2020-12-09 16:13:18 +0100

    updating repo

--- selenium/login.py
+++ selenium/login.py
@@ -1,7 +1,9 @@
 # #!/usr/bin/env python
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options  # as ChromeOptions, Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 import logging
@@ -10,19 +12,23 @@ import logging
 def login(user, password):
     # Create and configure logger
     logging.basicConfig(filename="login_log.log",
-                        format='%(asctime)s %(message)s',
-                        filemode='w')
+                            format='%(asctime)s %(message)s',
+                            filemode='w')
     # Creating an object
     logger = logging.getLogger()
+    options = ChromeOptions()
+    options.add_argument("--headless")
+    #driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
+    #driver = webdriver.Chrome(ChromeDriverManager(version="87.0.4280.88").install())
+    driver = webdriver.Chrome(ChromeDriverManager(version="87.0.4280.88", chrome_type=ChromeType.CHROMIUM).install())
     # Setting the threshold of logger to DEBUG
     logger.setLevel(logging.DEBUG)
     print('Starting the browser...')
     logger.debug('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    chromeOptions = Options()
-    chromeOptions.headless = True
-    driver = webdriver.Chrome(executable_path="C:/webdriver/chromedriver.exe", options=chromeOptions)
-
+    
+    options.add_argument("--headless")
+    
     print('Browser started successfully. Navigating to the demo page to login.')
     logger.debug('Browser started successfully. Navigating to the demo page to login.')
     # go to the page
@@ -34,27 +40,48 @@ def login(user, password):
     mypassword.send_keys(password)
     driver.find_element_by_id("login-button").click()
     if WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "inventory_container"))):
+        print('--------------------------------------------------------------------')
         print('The Login was successful by the user name: ' + user)
+        print('--------------------------------------------------------------------')
         logger.debug('The Login was successful by the user name: ' + user)
     else:
         print('Your login was unsuccessful')
         logger.debug('Your login was unsuccessful')
         driver.close()
+    # List of inventory
+    invenrotyList = driver.find_elements_by_class_name("inventory_list")
+    # List of items
+    itemList = driver.find_elements_by_class_name("inventory_item_name")
+    # List of add-buttons
     btnAddList = driver.find_elements_by_css_selector("button[class='btn_primary btn_inventory']")
-    # adding all products to the cart
-    for element in btnAddList:
-        element.click()
-    print('adding all products to a cart')
-    logger.debug('adding all products to a cart')
+    # List of remove-buttons
+    btnRemoveList = driver.find_elements_by_css_selector("button[class='btn_secondary btn_inventory']")
 
-    # List of buttons of all products
-    btnAddList = driver.find_elements_by_css_selector("button[class='btn_secondary btn_inventory']")
-    # removing all products from the cart
-    for element in btnAddList:
-        element.click()
-    print('removing all products from a cart')
-    logger.debug('removing all products from a cart')
+    # adding the products
+    for inventory in invenrotyList:
+        for item in itemList:
+            print("Adding " + item.text + " to cart")
+            logger.debug("Adding " + item.text + " to cart")
+        for btn in btnAddList:
+            btn.click()
+
+    print('----------------------------------')
+    print('All the products are added to cart')
+    logger.debug('All the products are added to cart')
+    print('----------------------------------------------------')
+    ## removing the products
+    for inventory in invenrotyList:
+        for item in itemList:
+            print("Removing " + item.text + " from cart")
+            logger.debug("Removing " + item.text + " from cart")
+        for btn in btnRemoveList:
+            btn.click()
+    print('----------------------------------------------------')
+    print('All products are removed from cart')
+    logger.debug('All products are removed from cart')
     driver.close()
 
-
 login('standard_user', 'secret_sauce')
+
+
+
